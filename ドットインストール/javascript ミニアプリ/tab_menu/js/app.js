8bd27226039942acd50eab08a7eb323aa9ec6e57
masanarih0ri <masanarihoridevelop@gmail.com>
2016-07-20 20:25:04 +0900

    ミニプログラム追加

---
+++ ドットインストール/javascript ミニアプリ/tab_menu/js/app.js
@@ -0,0 +1,27 @@
+(function(){
+	'use strict';
+
+	var menuItems = document.getElementsByClassName('menu_item');
+	var contents = document.getElementsByClassName('content');
+
+	var i;
+	
+	for(i = 0; i < menuItems.length; i++){
+		console.log(menuItems[i]);
+		menuItems[i].addEventListener('click',function(e){
+			e.preventDefault();
+			var i;
+			for(i = 0; i < menuItems.length; i++){
+				menuItems[i].className = 'menu_item';
+			}
+			this.className = 'menu_item active';
+
+			for(i = 0; i < contents.length; i++){
+				contents[i].className = 'content';
+			}
+			// document.getElementById(this.dataset.id).className = 'content active';
+			document.getElementById(this.dataset.id).className = 'content active';
+		});
+	}
+})();
+
